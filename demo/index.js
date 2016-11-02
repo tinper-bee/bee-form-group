84f869aac978c5ce67c5641aef0726fece7a0c4f
“liurhc” <liurhc@yonyou.com>
2016-11-02 14:57:29 +0800

    新增例子

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,36 @@
-import Demo from './FormGroupDemo';
+import FormGroup from '../src';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<Demo/>, document.getElementById('tinperBeeDemo'));
+import FormControl from 'bee-form-control';
+
+
+function demo1() {
+    return (
+        <div className="demo-form-group">
+        	<FormGroup>
+        		<FormControl />
+        	</FormGroup>
+        </div>
+    );
+}
+
+function demo2() {
+    return (
+        <div className="demo-form-group">
+        	<FormGroup validationState="error"> 
+				<FormControl type="text" />
+			</FormGroup>	
+			<FormGroup validationState="warning"> 
+				<FormControl type="text" />
+			</FormGroup>	
+			<FormGroup validationState="success"> 
+				<FormControl type="text" />
+			</FormGroup>			
+        </div>
+    );
+}
+
+
+
+ReactDOM.render(demo1(), document.getElementById('ReactFormGroupDemo1'));
+ReactDOM.render(demo2(), document.getElementById('ReactFormGroupDemo2'));
