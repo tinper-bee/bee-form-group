5d6000ec27a7006413f33fd6fd5ae21dda0efadc
“liurhc” <liurhc@yonyou.com>
2016-11-03 10:07:19 +0800

    ver. 1.0.2

--- build/FormGroup.js
+++ build/FormGroup.js
@@ -77,17 +77,3 @@ FormGroup.propTypes = propTypes;
 
 exports["default"] = FormGroup;
 module.exports = exports['default'];
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _FormGroup = require('./FormGroup');
-
-var _FormGroup2 = _interopRequireDefault(_FormGroup);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-exports["default"] = _FormGroup2["default"];
-module.exports = exports['default'];
